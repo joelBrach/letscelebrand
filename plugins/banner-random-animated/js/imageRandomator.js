3523ee6e887b494dbf90208d22c083c7f3c0c0cb
obodrov <bodrovphone@gmail.com>
2017-08-31 21:24:31 +0200

    refactoring Imagerandomator and fixing html heigth bug

--- plugins/banner-random-animated/js/imageRandomator.js
+++ plugins/banner-random-animated/js/imageRandomator.js
@@ -1,15 +1,82 @@
-/* <![CDATA[ */
-var hero_images = [{"key":197,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Citroen-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Citroen-1.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Citroen-2.jpg?w=1200&auto=format"]},{"key":199,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-1.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-2-e1441239838142.jpg?w=1200&auto=format"]},{"key":201,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-1.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-2.jpg?w=1200&auto=format"]},{"key":211,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-1.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-2.jpg?w=1200&auto=format"]},{"key":9473,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/17004143\/Launch-5677.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/17004143\/Launch-5677.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Launch-5789.jpg?w=1200&auto=format"]},{"key":10048,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_PH_Full2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_PH_Full2.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_3DW_Crop.jpg?w=1200&auto=format"]},{"key":10050,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_002.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_002.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_008.jpg?w=1200&auto=format"]},{"key":10059,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/AP_Deodorant_HR_2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/AP_Deodorant_HR_2.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/29132043\/Book_03.jpg?w=1200&auto=format"]},{"key":10052,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/alitalia.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/alitalia.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Alitalia-Anitmacassar.jpg?w=1200&auto=format"]},{"key":13543,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/19160718\/SP-lobby_edit_2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/19160718\/SP-lobby_edit_2.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2017\/04\/17114902\/SP-Floor.jpg?w=1200&auto=format"]},{"key":13547,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102504\/SDI2896-RGB.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102504\/SDI2896-RGB.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102535\/SDI3212-RGB.jpg?w=1200&auto=format"]},{"key":13549,"images":["https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104100\/Melbourne-1-edit.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104100\/Melbourne-1-edit.jpg?w=1200&auto=format","https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104105\/Landor_Folio_Australian-Open_288.jpg?w=1200&auto=format"]}];
-/* ]]> */
-
-
+var hero_images = [{
+    "key": 197,
+    "images": [
+    "https://pro2-bar-s3-cdn-cf6.myportfolio.com/d110318fa440a2415bde0089b3da7f5f/aacc0c38232893.5759b51b43505_rwc_377x82x2734x2140x2734.jpg?h=b2d3f8930400f32e10e323acfcd9b3cd", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Citroen-1.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Citroen-2.jpg?w=1200&auto=format"]
+}, {
+    "key": 199,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-1.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/CPC-2-e1441239838142.jpg?w=1200&auto=format"]
+}, {
+    "key": 201,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-1.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Etihad-2.jpg?w=1200&auto=format"]
+}, {
+    "key": 211,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-1.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-1.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Melbourne-2.jpg?w=1200&auto=format"]
+}, {
+    "key": 9473,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/17004143\/Launch-5677.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/17004143\/Launch-5677.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Launch-5789.jpg?w=1200&auto=format"]
+}, {
+    "key": 10048,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_PH_Full2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_PH_Full2.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/OC_3DW_Crop.jpg?w=1200&auto=format"]
+}, {
+    "key": 10050,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_002.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_002.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/M_G_Stationery_008.jpg?w=1200&auto=format"]
+}, {
+    "key": 10059,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/AP_Deodorant_HR_2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/AP_Deodorant_HR_2.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2016\/03\/29132043\/Book_03.jpg?w=1200&auto=format"]
+}, {
+    "key": 10052,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/alitalia.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/alitalia.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/27135714\/Alitalia-Anitmacassar.jpg?w=1200&auto=format"]
+}, {
+    "key": 13543,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/19160718\/SP-lobby_edit_2.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/19160718\/SP-lobby_edit_2.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2017\/04\/17114902\/SP-Floor.jpg?w=1200&auto=format"]
+}, {
+    "key": 13547,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102504\/SDI2896-RGB.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102504\/SDI2896-RGB.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20102535\/SDI3212-RGB.jpg?w=1200&auto=format"]
+}, {
+    "key": 13549,
+    "images": [
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104100\/Melbourne-1-edit.jpg?w=1200&sat=-100&blend=FED100&bm=multiply&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104100\/Melbourne-1-edit.jpg?w=1200&auto=format", 
+    "https:\/\/landor-prod.imgix.net\/app\/uploads\/2015\/08\/20104105\/Landor_Folio_Australian-Open_288.jpg?w=1200&auto=format"]
+}];
 (function imageRandomator() {
-  var first = Math.ceil(Math.random() * 11);
-  var second = Math.ceil(Math.random() * 11);
+  var random = Math.ceil(Math.random() * 11);
   var heroImages = $('.home-hero__image');
 
-  heroImages[0].style.backgroundImage = "url(" + hero_images[first].images[0] + ")";
-  heroImages[1].style.backgroundImage = "url(" + hero_images[first].images[1] + ")";
-  heroImages[2].style.backgroundImage = "url(" + hero_images[second].images[2] + ")";
+  heroImages[0].style.backgroundImage = "url(" + hero_images[0].images[0] + ")";
+  heroImages[1].style.backgroundImage = "url(" + hero_images[random].images[1] + ")";
+  heroImages[2].style.backgroundImage = "url(" + hero_images[random].images[2] + ")";
 
 })();
